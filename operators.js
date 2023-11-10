ffd7f4a148f91495df445eca494040f3728c6b14
eeshurtliff <eeshurtliff@gmail.com>
2023-11-09 19:29:40 -0700

    worked on operations

---
+++ operators.js
@@ -0,0 +1,20 @@
+// Add two numbers
+let x = 2 + 5;
+// Multiply two numbers
+let y = 2 * 5;
+// Subtract one from a number
+let a = 4 - 1;
+// concatinate two strings together.
+let CombinedStrings = "Hello" + "World";
+// assign a value to a variable
+let z = 4;
+// increment the value in a variable by 3
+z = z + 3;
+// compare two values to see if they are the same
+let isSame = x == y;
+// check to see if one number is less than another number
+let isLess = a <x;
+// Check to see if two values are NOT equal
+let notSame = x != y;
+// check to see if a value is less than 10 and greater than 0
+let compare = 10 > x & x > 0;
