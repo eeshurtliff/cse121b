e1b1eaed4dd38fe17e97efe1beaf1720edd8af81
eeshurtliff <eeshurtliff@gmail.com>
2023-11-19 22:48:26 -0700

    completed week 3

--- w03-task/w03-task.js
+++ w03-task/w03-task.js
@@ -1,5 +1,4 @@
 /* LESSON 3 - Programming Tasks */
-
 /* FUNCTIONS */
 /* Function Definition - Add Numbers */
 function add(number1, number2){
@@ -7,7 +6,7 @@ function add(number1, number2){
 }
 
 function addNumbers(){
-    let addNumber1 = Number(document.querySelector('#add1'). value);
+    let addNumber1 = Number(document.querySelector('#add1').value);
     let addNumber2 = Number(document.querySelector('#add2').value);
     document.querySelector('#sum').value = add(addNumber1, addNumber2);
 }
@@ -35,8 +34,8 @@ const multiply = (number1, number2) => {
 }
 
 const multiplyNumbers = (multiplyNumber1, multiplyNumber2) => {
-    let multiplyNumber1 = Number(document.querySelector('#factor1').value);
-    let multiplyNumber2 = Number(document.querySelector('#factor2').value);
+     multiplyNumber1 = Number(document.querySelector('#factor1').value);
+     multiplyNumber2 = Number(document.querySelector('#factor2').value);
     document.querySelector('#product').value = multiply(multiplyNumber1, multiplyNumber2);
 
 }
@@ -60,23 +59,72 @@ document.querySelector('#divideNumbers').addEventListener('click', divideNumbers
 
 /* Decision Structure */
 
-let subtotal = Number(document.querySelector('#subtotal').value);
+/* let subtotal = Number(document.querySelector('#subtotal').value);*/
 
-document.querySelector('#getTotal').addEventListener('click', subtotal);
+console.log('2jskdjfls')
 
-let 
-if
+
+function applyDiscount(){
+    
+    let membershipCheckbox = document.getElementById('member');
+    let subtotalAmount = Number(document.querySelector('#subtotal').value);
+    console.log(subtotalAmount);
+    let totalAmountSelector = Number(document.querySelector('#total').value);
+    let totalReturn = document.querySelector('#total');
+    
+    if(membershipCheckbox.checked) {
+        let discount = 0.15 * subtotalAmount;
+        let discountedAmount = subtotalAmount - discount;
+        totalReturn.innerHTML = discountedAmount;
+        
+    }else(
+        totalReturn.innerHTML = subtotalAmount
+    )
+}
+
+let totalButton = document.querySelector('#getTotal').addEventListener('click', applyDiscount);
+
+
 
 
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
 
+let numbersArray = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+arrayElement = document.querySelector('#array');
+arrayElement.innerHTML = numbersArray;
+
 /* Output Odds Only Array */
+document.querySelector('#odds').innerHTML = numbersArray.filter(number => number % 2 != 0);
+
 
 /* Output Evens Only Array */
 
+document.querySelector('#evens').innerHTML = numbersArray.filter(number => number % 2 == 0);
+
 /* Output Sum of Org. Array */
+
+let reducedArray = numbersArray.reduce((sum, number) => sum + number, 0).toString();
+console.log(reducedArray)
+let arraySum = document.getElementById('sumOfArray').innerHTML = reducedArray;
+
+
 
 /* Output Multiplied by 2 Array */
 
+
+multipliedArray = document.querySelector('#multiplied');
+multipliedArray.innerHTML = numbersArray.map(number => number * 2);
+
+
 /* Output Sum of Multiplied by 2 Array */
+
+// let reducedMultipliedArray = multipliedArray.reduce((sum, number) => sum + number);
+// document.querySelector('#sumOfMultiplied').innerHTML = reducedMultipliedArray;
+
+// document.querySelector('sumOfMultiplied').innerHTML = numbersArray.reduce((sum) => sum + numbersArray.map(number => number *2), 0);
+
+let doubledArray = numbersArray.map(number => number * 2);
+let reducedDoubledArray = doubledArray.reduce((sum, number) => sum + number, 0);
+
+let SumOfDoubledArray = document.querySelector('#sumOfMultiplied').innerHTML = reducedDoubledArray;
